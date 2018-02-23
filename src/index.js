9b8a3b1491770824aed6dffce7fdd1e0feca0103
Jeriko <jcarrico1985@live.com>
2018-02-23 15:27:00 -0600

    added loading animation. begin initial building of html from api request.

--- src/index.js
+++ src/index.js
@@ -7,14 +7,36 @@ sayHello('World');
 /**
  * require style imports
  */
+
+const $ = require('jquery');
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
+  let htmlString = '';
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+    htmlString += `<div>${title}</div>
+                   <div>Rating: ${rating}/5</div>`
   });
+  $('.container').html(htmlString);
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+
+const loadAnimate = () => {
+  let loadText = $('#loading').text();
+  let i = 0;
+  setInterval(function() {
+    if(i === 3) {
+      i = 0;
+      loadText = 'Loading';
+    }
+    loadText += '.';
+    $('#loading').text(loadText);
+    i++;
+  }, 1000)
+};
+
+loadAnimate();
